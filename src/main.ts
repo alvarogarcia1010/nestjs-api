437a159ec90efe3c07851cd32be29d67e536c94a
alvarogarcia1010 <00111317@uca.edu.sv>
2024-09-03 22:05:31 -0600

    [ADDED] Enabled Cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-  })
+  app.enableCors()
   await app.listen(3000)
 }
 
